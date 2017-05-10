4ef2422f2f844f75a3dc997ea9f83ba9dba23cba
Ksenia D <Ksenia D>
2017-05-10 15:30:04 -0400

    front-end files

---
+++ js/front.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+     console.log("hello");
+	 $('#second').hide();
+	 $('#third').hide();
+
+	 
+	$('#submit').click(function(event){
+		event.preventDefault();
+
+		 $('#second').show(function() {
+			 $("#second").animate({right: '0vw'}, 1000);
+			 $("#first").animate({right: '100vw'}, 1000, function(){
+				 $('#first').hide();
+			 });
+		 });
+		 });
+	
+	$('#submit2').click(function(event){
+		event.preventDefault();
+
+
+			$('#third').show(function() {
+			 $("#third").animate({left: '0vw'}, 1000);
+			 $("#first").animate({left: '100vw'}, 1000, function(){
+				 $('#first').hide();
+			 });
+		 });
+		}); 
+
+});
